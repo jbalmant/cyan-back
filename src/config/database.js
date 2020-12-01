19a6d706f46e9ddc2a6b1ecaec3ee98843e84a12
Jonatas Balmant <jbalmant@live.com>
2020-12-01 05:01:05 -0300

    fixing config

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,21 @@
 module.exports = {
-    dialect: 'postgres',
-    // host: 'localhost',
-    // username: 'admin',
-    // password: 'secret',
-    // database: 'cyan',
-    define: {
-        timestamps: true,
-        underscored: true
+    "development": {
+        dialect: 'postgres',
+        host: 'localhost',
+        username: 'admin',
+        password: 'secret',
+        database: 'cyan',
+        define: {
+            timestamps: true,
+            underscored: true
+        }
     },
-    use_env_variable: "DATABASE_URL"
-}
+    "production": {
+        dialect: 'postgres',
+        define: {
+            timestamps: true,
+            underscored: true
+        },
+        use_env_variable: "DATABASE_URL"
+    }
+};
