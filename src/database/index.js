f57f5933cf34356640de2db3d3d61564774d9c0a
Jonatas Balmant <jbalmant@live.com>
2020-12-01 04:17:34 -0300

    adding db

--- src/database/index.js
+++ src/database/index.js
@@ -4,7 +4,19 @@ const dbConfig = require('../config/database');
 const File = require('../models/File');
 const Location = require('../models/Location');
 
-const connection = new Sequelize(dbConfig);
+let connection = null
+
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect:  'postgres',
+        protocol: 'postgres',
+        port:     match[4],
+        host:     match[3],
+        logging:  true //false
+      })
+} else {
+    new Sequelize(dbConfig);
+}
 
 File.init(connection);
 Location.init(connection);
