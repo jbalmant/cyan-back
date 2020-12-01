f36342ad86028b5e696604acd17b21dd82617007
Jonatas Balmant <jbalmant@live.com>
2020-12-01 06:12:06 -0300

    wip

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const dbConfig = require('../config/database');
+const dbConfig = require('../config/database.json');
 
 const File = require('../models/File');
 const Location = require('../models/Location');
@@ -7,7 +7,10 @@ const Location = require('../models/Location');
 console.log("ENV", process.env);
 
 
-const connection = new Sequelize(dbConfig);
+const connection = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres'
+});
 
 File.init(connection);
 Location.init(connection);
