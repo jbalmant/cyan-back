fc1701e0807f623493080c509420f0a601985b13
Jonatas Balmant <jbalmant@live.com>
2020-12-01 06:48:21 -0300

    Heroku working (#2)

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -4,7 +4,7 @@ const download = require('../utils/download');
 
 const url = require("url");
 const Fs = require('fs');
-const csv = require ('fast-csv');
+const csv = require('fast-csv');
 const Path = require('path')
 
 // TODO (jbalmant) - Move to a better place.
@@ -16,23 +16,23 @@ importCSV = (file, filename) => {
 
     const locations = [];
 
-    File.update( {status: FileStatus.IMPORTING}, { where: {id: file.id}});
+    File.update({ status: FileStatus.IMPORTING }, { where: { id: file.id } });
 
     Fs.createReadStream(path)
         .pipe(csv.parse({ headers: true }))
         .on('error', error => {
             console.error(error);
-            File.update( {status: FileStatus.IMPORTING_ERROR}, { where: {id: file.id}});
+            File.update({ status: FileStatus.IMPORTING_ERROR }, { where: { id: file.id } });
             throw error.message;
         })
         .on('data', row => {
-            locations.push({lat: row.latitude, lng: row.longitude, file_id: file.id});
+            locations.push({ lat: row.latitude, lng: row.longitude, file_id: file.id });
         })
         .on('end', () => {
             Location.bulkCreate(locations).then(() => {
                 console.log('Successfully imported')
-                File.update( {status: FileStatus.IMPORTED}, { where: {id: file.id}});
-            });    
+                File.update({ status: FileStatus.IMPORTED }, { where: { id: file.id } });
+            });
         });
 }
 
@@ -41,21 +41,24 @@ importCSV = (file, filename) => {
 // TODO (jbalmant) - Study if is possible to download directly to database.
 processFile = (file) => {
     console.log(`Downloading ${file.path}`);
-    File.update( {status: FileStatus.DOWNLOADING}, { where: {id: file.id}});
+    File.update({ status: FileStatus.DOWNLOADING }, { where: { id: file.id } });
 
-    download(file.path, 'somename').then(function() {
-        File.update( {status: FileStatus.DOWNLOADED}, { where: {id: file.id}});
-        importCSV(file, 'somename')
+    const filename = Date.now() + file.filename
+
+    // TODO (jbalmant) - Create temp filename
+    download(file.path, filename).then(function () {
+        File.update({ status: FileStatus.DOWNLOADED }, { where: { id: file.id } });
+        importCSV(file, filename)
         console.log('Download Done');
-    }).catch(function(err) {
+    }).catch(function (err) {
         console.log(`An error occured ${err}`)
-        File.update( {status: FileStatus.DOWNLOADING_ERROR}, { where: {id: file.id}});
+        File.update({ status: FileStatus.DOWNLOADING_ERROR }, { where: { id: file.id } });
     });
-    
+
 }
 
 // TODO (jbalmant) - How to use enum on NodeJS?
-const FileStatus = Object.freeze({"QUEUED":1, "DOWNLOADING":2, "DOWNLOADED":3, "IMPORTING":4, "IMPORTED":5, "DOWNLOADING_ERROR": -1, "IMPORTING_ERROR": -2});
+const FileStatus = Object.freeze({ "QUEUED": 1, "DOWNLOADING": 2, "DOWNLOADED": 3, "IMPORTING": 4, "IMPORTED": 5, "DOWNLOADING_ERROR": -1, "IMPORTING_ERROR": -2 });
 
 module.exports = {
     async list(req, res) {
--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,17 @@
 const Sequelize = require('sequelize');
-const dbConfig = require('../config/database');
+const dbConfig = require('../config/database.json');
 
 const File = require('../models/File');
 const Location = require('../models/Location');
 
-const connection = new Sequelize(dbConfig);
+const connection = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+});
 
 File.init(connection);
 Location.init(connection);
